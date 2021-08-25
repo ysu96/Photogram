88ebdf0f767a17ce3e108a5c849fa4014dba8b94
ysu96 <ysu96@naver.com>
2021-08-25 23:50:22 +0900

    subscribe list

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,10 +1,13 @@
 package com.cos.photogramstart.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,4 +33,12 @@ public class SubscribeService {
 		// 삭제는 오류 날 일이 없음, 삭제 실패해도 오류 안남
 	}
 
+
+	@Transactional(readOnly = true)
+	public List<SubscribeDto> 구독리스트(int principalId, int pageUserId){
+		
+		
+		return null;
+	}
+	
 }
